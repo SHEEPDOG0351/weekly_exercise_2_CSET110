d77374eb71500919144fe41d064a50ddb80cdfdd
SHEEPDOG0351 <michaelwork2006@gmail.com>
2024-10-17 08:39:07 -0400

    Finished add button functionality

--- javascript_store.js
+++ javascript_store.js
@@ -23,12 +23,6 @@ function ready(){
     }
 }
 
-function removeCartItem(event) {
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
-    cartTotal()
-}
-
 function quantityChanged(event) {
     var input = event.target
     if (isNaN(input.value) || input.value <= 0) {
@@ -36,34 +30,50 @@ function quantityChanged(event) {
     }
     cartTotal()     
 }
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    cartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemsNames = cartItems.getElementsByClassName('cart-item-title')
+    for (i = 0; i < cartItemsNames.length; i++) {
+        if (cartItemsNames[i].innerText === title) {
+            alert('This item is already added to the cart')
+            return
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+        cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
     // Loop through each button and attach the event listener
     // Used for remove buttons
     for (let i = 0; i < remove_buttons.length; i++) {
         remove_buttons[i].addEventListener('click', remove_items);
     }
 
-    // Adds add to cart button functionality
-    function add_items(event) {
-
-        // Finds button clicked
-        const add_button_clicked = event.target
-
-        // Access the container div for the row
-        const row_div = document.querySelector('div.cart-row');
-
-        // Add new item before div
-        row_div.insertAdjacentHTML('beforebegin', '');
-
-
-
-        // Add necessary content to the rows class for the new item
-        cart_rows.innerHTML += 
-
-        // Update cart total after addition of new item
-        cartTotal()
-    }
-
-    function remove_items(event) {
+    function removeCartItem(event) {
         // Find the button that was clicked
         const button_clicked = event.target;
 
